92fe203d3326016acb37f2fbf1fac4a25b7e9755
watch14 <watchman1414@gmail.com>
2024-05-05 07:07:49 +0100

    categories done

--- Petsy/src/app/app.component.ts
+++ Petsy/src/app/app.component.ts
@@ -2,11 +2,16 @@ import { CarouselleComponent } from './carouselle/carouselle.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CategorysComponent } from './categorys/categorys.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarouselleComponent,],
+  imports: [RouterOutlet, 
+            HeaderComponent, 
+            CarouselleComponent,
+            CategorysComponent,
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Petsy/src/app/categorys/categorys.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorys',
+  standalone: true,
+  imports: [],
+  templateUrl: './categorys.component.html',
+  styleUrl: './categorys.component.css'
+})
+export class CategorysComponent {
+
+}
