2f47632f2a9edfdd0219fb6c0d7178d9f9538a03
watch14 <watchman1414@gmail.com>
2024-05-06 08:03:50 +0100

    filter work in progress

--- Petsy/src/app/app.component.ts
+++ Petsy/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CategorysComponent } from './categorys/categorys.component';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { CategorysComponent } from './categorys/categorys.component';
             HeaderComponent, 
             CarouselleComponent,
             CategorysComponent,
+            FilterComponent
           ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
