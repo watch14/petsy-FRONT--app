83c24fcbcf8863ce402e84722b080dafd890ec52
watch14 <watchman1414@gmail.com>
2024-05-11 13:15:39 +0100

    blogs page redirect to blog

--- Petsy/src/app/blog/blog.component.ts
+++ Petsy/src/app/blog/blog.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogsPageComponent } from '../blogs-page/blogs-page.component';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [BlogsPageComponent,
+            CommonModule
+  ],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  blog: any;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // Retrieve the blog data passed from the previous component
+    this.blog = history.state.blogData;
+
+    // Log the title of the blog
+    console.log('Blog Title:', this.blog.blogName);
+    console.log("B1111111111111");
+  }
 }
--- Petsy/src/app/blogs-page/blogs-page.component.ts
+++ Petsy/src/app/blogs-page/blogs-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -15,6 +16,9 @@ import { RouterLink, RouterModule } from '@angular/router';
 })
 export class BlogsPageComponent {
 
+  constructor(private router: Router) {}
+
+
   blogs: {blogPicture: string, blogName: string, blogContent: string}[] = [
     {blogPicture: "../../assets/images/blog-images/blog_image (1).png", blogName: "Unveiling the Secrets of Pet Nutrition: A Guide to Optimal Health", blogContent: "Embark on a journey to unlock the secrets of pet nutrition, delving deep into the foundations of optimal health. Discover the power of balanced diets, essential nutrients, and personalized feeding plans, paving the way for vibrant well-being and longevity in our beloved furry companions. Let this comprehensive guide be your compass towards nourishing their bodies and nurturing their spirits, fostering a lifetime of vitality and joy. Unravel the mysteries of pet behavior and gain valuable insights into your furry friend's actions. From separation anxiety to aggression, delve into the underlying causes of behavioral issues and explore effective strategies for addressing them. With patience, understanding, and positive reinforcement, you can strengthen your bond with your pet and create a harmonious environment where both of you can thrive.Embark on a journey to unlock the secrets of pet nutrition, delving deep into the foundations of optimal health. Discover the power of balanced diets, essential nutrients, and personalized feeding plans, paving the way for vibrant well-being and longevity in our beloved furry companions. Let this comprehensive guide be your compass towards nourishing their bodies and nurturing their spirits, fostering a lifetime of vitality and joy. Unravel the mysteries of pet behavior and gain valuable insights into your furry friend's actions. From separation anxiety to aggression, delve into the underlying causes of behavioral issues and explore effective strategies for addressing them. With patience, understanding, and positive reinforcement, you can strengthen your bond with your pet and create a harmonious environment where both of you can thrive"},
     {blogPicture: "../../assets/images/blog-images/blog_image (2).png", blogName: "Common Pet Ailments: Understanding Symptoms and Treatment Options", blogContent: "Explore the common ailments that afflict our furry friends, from allergies to digestive issues. Learn to recognize the symptoms and understand the available treatment options, empowering you to provide the best care for your pet's well-being and comfort. Arm yourself with knowledge and compassion to navigate through health challenges and ensure a happy, healthy life for your beloved companion."},
@@ -22,7 +26,9 @@ export class BlogsPageComponent {
     {blogPicture: "../../assets/images/blog-images/blog_image (4).png", blogName: "Decoding Pet Behavior: Insights into Understanding and Addressing Behavioral Issues", blogContent: "Unravel the mysteries of pet behavior and gain valuable insights into your furry friend's actions. From separation anxiety to aggression, delve into the underlying causes of behavioral issues and explore effective strategies for addressing them. With patience, understanding, and positive reinforcement, you can strengthen your bond with your pet and create a harmonious environment where both of you can thrive."},
 ];
 
-
+redirectToBlog(blog: any) {
+  this.router.navigate(['/blog'], { state: { blogData: blog } });
+}
 
 
 }
