634d63f5b97c3dd5013eeae46964189143b80850
watch14 <watchman1414@gmail.com>
2024-05-11 12:37:44 +0100

    blog page + fixed

--- Petsy/src/app/app.routes.ts
+++ Petsy/src/app/app.routes.ts
@@ -1,12 +1,15 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CategorysComponent } from './categorys/categorys.component';
 import { FilterComponent } from './filter/filter.component';
 import { BlogsPageComponent } from './blogs-page/blogs-page.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
     {'path': '', component:HomePageComponent},
     {'path': 'animal_species', component:CategorysComponent},
     {'path': 'animal_filter', component:FilterComponent},
     {'path': 'blogs', component:BlogsPageComponent},
+    {'path': 'blog', component:BlogComponent},
 ];
