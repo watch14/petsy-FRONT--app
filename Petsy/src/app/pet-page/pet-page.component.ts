9e62bb8fc932cec4cd563b223a7dc1ebde331749
watch14 <watchman1414@gmail.com>
2024-05-22 19:29:14 +0100

    pet html / css -> almost done 👌

--- Petsy/src/app/pet-page/pet-page.component.ts
+++ Petsy/src/app/pet-page/pet-page.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pet-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-page.component.html',
   styleUrl: './pet-page.component.css'
 })
 export class PetPageComponent {
+  about = ["Breed", "Age", "Gender", "Color", "Health Status", "Temperament", "Size"]
+  aboutInfo = ["Labrador Retriever", "2 months", "Male", "White", "Vaccinated", "Playful ...", "Small"]
+
+  owner: {icone: string, ownerInfo: string}[] = [
+    {icone : "../../assets/images/vectors/account.svg", ownerInfo : "ownerInfo"},
+    {icone : "../../assets/images/vectors/location.svg", ownerInfo :"123 Jasmine Street, Tunis, Tunisia"},
+    {icone : "../../assets/images/vectors/call.svg", ownerInfo : "+217 27 600 300"}
+  ];
+
+
 
 }
