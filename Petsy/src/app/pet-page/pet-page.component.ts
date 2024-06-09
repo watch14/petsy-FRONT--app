b7ade8a3f99fcdc7afdbc503afa18f7a345fe970
watch14 <watchman1414@gmail.com>
2024-06-09 13:47:47 +0100

    pet gape on the way

--- Petsy/src/app/pet-page/pet-page.component.ts
+++ Petsy/src/app/pet-page/pet-page.component.ts
@@ -13,7 +13,7 @@ export class PetPageComponent {
   aboutInfo = ["Labrador Retriever", "2 months", "Male", "White", "Vaccinated", "Playful ...", "Small"]
 
   owner: {icone: string, ownerInfo: string}[] = [
-    {icone : "../../assets/images/vectors/account.svg", ownerInfo : "ownerInfo"},
+    {icone : "../../assets/images/vectors/account.svg", ownerInfo : "Monsef Bay"},
     {icone : "../../assets/images/vectors/location.svg", ownerInfo :"123 Jasmine Street, Tunis, Tunisia"},
     {icone : "../../assets/images/vectors/call.svg", ownerInfo : "+217 27 600 300"}
   ];
